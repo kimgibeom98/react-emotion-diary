71b6caaa1d4c4501af732db5caf4eb8e6b9785eb
PC <kim000183@naver.com>
2022-12-15 23:56:24 +0900

    useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,17 +1,20 @@
-import { useSearchParams } from "react-router-dom";
+import {useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setsearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
 
-    const [searchParams, setsearchParams] = useSearchParams();
-    const id = searchParams.get("id");
-    const mode = searchParams.get("mode");
-    
-    return (
-        <div>
-            <h1>Edit</h1>
-            <p>이곳은 일기 수정페이지 입니다.</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정페이지 입니다.</p>
+      <button onClick={() => setsearchParams({ who: 'gibeom', mode: 'dark' })}>QS 바꾸기</button>
+      <button onClick={() => {navigate('/home')}}>HOME으로 가기</button>
+      <button onClick={() => {navigate(-1)}}>뒤로가기</button>
+    </div>
+  )
 };
 
 export default Edit;
