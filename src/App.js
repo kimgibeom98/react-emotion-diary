5254bb0fa345c838cb3ed678a2bc3022a6f46c88
PC <kim000183@naver.com>
2022-12-18 20:24:00 +0900

    img추가 및 test파일 제거

--- src/App.js
+++ src/App.js
@@ -6,20 +6,27 @@ import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
-import RoutTest from './components/RouteTest';
-
 function App() {
+
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || ""; 
+
   return (
     <BrowserRouter>
       <div className='App'>
         <h2>App.js</h2>
+
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`}/>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`}/>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`}/>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`}/>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`}/>
         <Routes>
           <Route path='/' element = {<Home />} />
           <Route path='/new' element = {<New />} />
           <Route path='/edit' element = {<Edit />} />
           <Route path='/diary/:id' element = {<Diary />} />
         </Routes>
-        <RoutTest />
       </div>
     </BrowserRouter>
   )
