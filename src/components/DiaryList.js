dc9b62a82a0bd442fa350d4d7092b53c1958c015
PC <kim000183@naver.com>
2022-12-21 23:32:11 +0900

    리스트 정렬 진행중

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,6 +1,26 @@
+import { useState } from "react"
+
+const sortOptionList = [
+  {value : "lastest", name:"최신순"},
+  {value : "oldest", name:"오래된 순"}
+]
+
+
+const ControlMenu = ({value,onChange,optionList}) => {
+  return(
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it,idx) => 
+      <option key={idx} value={it.value}>{it.name}</option>)}
+    </select>
+   )
+}
+
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState('lastest');
+
   return (
   <div>
+    <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
     {diaryList.map((it) => (<div key={it.id}> {it.content} </div>))}
   </div>
   )
