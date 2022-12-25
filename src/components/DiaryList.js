767c1ad2112f2ac42da0c97f00ccab83562ec81c
PC <kim000183@naver.com>
2022-12-25 17:41:07 +0900

    진행중

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -18,10 +18,24 @@ const ControlMenu = ({value,onChange,optionList}) => {
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState('lastest');
 
+
+  const getProcesedDiaryList = () => {
+    const compare = (a,b) => {
+      if(sortType === "lastest"){
+        return parseInt(b.date) - parseInt(a.date);
+      }else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    }
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+    return sortedList
+  } 
+
   return (
   <div>
     <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
-    {diaryList.map((it) => (<div key={it.id}> {it.content} </div>))}
+    {getProcesedDiaryList().map((it) => (<div key={it.id}> {it.content} </div>))}
   </div>
   )
 }
