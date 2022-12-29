7212be44460be4e176c7bf1f0ec0f3224ca1703d
PC <kim000183@naver.com>
2022-12-29 21:05:15 +0900

    오늘의 일기 style구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate} from "react-router-dom";
+import { useRef, useState } from "react";
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
@@ -39,6 +39,8 @@ const getStringDate = (date) => {
 }
 
 const DiaryEditor = () => {
+  const contentRef = useRef();
+  const [content,setContent] = useState('');
   const [emotion, setEmotion] = useState(3);
   const navigate = useNavigate();
 
@@ -65,7 +67,11 @@ const handleClickEmote = (emotion) => {
         </section>
         <section>
           <h4>오늘의 일기</h4>
-          <div className="input_box text_wrapper"></div>
+          <div className="input_box text_wrapper">
+            <textarea placeholder="오늘은 어땠나요" ref={contentRef} value={content} onChange={(e) => setContent(e.target.value)}>
+
+            </textarea>
+          </div>
         </section>
       </div>
     </div>
