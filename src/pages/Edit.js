cf137656bc2b18d2a765505384b192687235b80d
PC <kim000183@naver.com>
2022-12-30 15:47:41 +0900

    수정화면 진행중

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,18 +1,28 @@
-import {useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
 
 const Edit = () => {
+  const [originData, setOriginData] = useState();
   const navigate = useNavigate();
-  const [searchParams, setsearchParams] = useSearchParams();
-  const id = searchParams.get("id");
-  const mode = searchParams.get("mode");
+  const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id))
+      
+      if(targetDiary){
+
+      }else{
+        navigate("/",{replace:true})
+      }
+    }
+  }, [id, diaryList])
 
   return (
     <div>
-      <h1>Edit</h1>
-      <p>이곳은 일기 수정페이지 입니다.</p>
-      <button onClick={() => setsearchParams({ who: 'gibeom', mode: 'dark' })}>QS 바꾸기</button>
-      <button onClick={() => {navigate('/home')}}>HOME으로 가기</button>
-      <button onClick={() => {navigate(-1)}}>뒤로가기</button>
+      <h2>sdf</h2>
     </div>
   )
 };
