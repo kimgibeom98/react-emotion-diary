3bbed076f36a0a4acd33231923adf509718b1ce0
PC <kim000183@naver.com>
2023-01-02 21:14:51 +0900

    상세페이지 진행중

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,16 +1,41 @@
 
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+import { getStringDate } from "../util/date";
 
 const Diary = () => {
 
-    const {id} = useParams();
-    console.log(id)
+  const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data,setData] = useState();
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id))
+      
+      if(targetDiary){
+        setData(targetDiary)
+      }else{
+        alert('없는 일기입니다.');
+        navigate("/",{replace:true})
+      }
+    }
+  }, [id, diaryList])
+
+  if(!data){
+    return <div className="DiaryPage">로딩중입니다...</div>
+  }else{
     return (
-        <div>
-            <h1>Diary</h1>
-            <p>이곳은 일기 상세 페이지 입니다.</p>
-        </div>
+      <div className="DiaryPage">
+        <MyHeader headText={`${getStringDate(new Date(data.date))} 기록`}
+         leftChild={<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)} />}
+         rightChild={<MyButton text={'수정'}/>} onClick={() => navigate(`/edit/${data.id}`)}/>
+      </div>
     )
+  }
 };
 
 export default Diary;
