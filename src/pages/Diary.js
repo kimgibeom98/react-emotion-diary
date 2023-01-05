5a66522a24f8ffe9ffe37b65111187be23206171
PC <kim000183@naver.com>
2023-01-05 20:19:23 +0900

    favicon 및 페이지별 title 변경

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -15,16 +15,21 @@ const Diary = () => {
   const [data, setData] = useState();
 
   useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `감정 일기장 - ${id}번 일기`;
+  },[]);
+
+  useEffect(() => {
     if (diaryList.length >= 1) {
-      const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id))
+      const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id));
       if (targetDiary) {
-        setData(targetDiary)
+        setData(targetDiary);
       } else {
         alert('없는 일기입니다.');
-        navigate("/", { replace: true })
+        navigate("/", { replace: true });
       }
     }
-  }, [id, diaryList])
+  }, [id, diaryList]);
 
   if (!data) {
     return <div className="DiaryPage">로딩중입니다...</div>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -11,17 +11,22 @@ const Edit = () => {
   const diaryList = useContext(DiaryStateContext);
 
   useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `감정 일기장 - ${id}번 일기 수정`;
+  },[]);
+
+  useEffect(() => {
     if (diaryList.length >= 1) {
       const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id))
 
       if (targetDiary) {
-        setOriginData(targetDiary)
+        setOriginData(targetDiary);
       } else {
         alert('없는 일기입니다.');
-        navigate("/", { replace: true })
+        navigate("/", { replace: true });
       }
     }
-  }, [id, diaryList])
+  }, [id, diaryList]);
 
   return (
     <div>
